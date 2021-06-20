8121c52f20d00bba761b825b9dea0186ee4544fd
Easy-System-EF <enelmofreitas@gmail.com>
2021-06-20 17:48:51 -0300

    Order, Instant, ISO 8601 01

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,14 +1,20 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 // mapeamento obj p/ bco dados relacional
 @Entity
+//anotação para conflito de palavras reservadas, ou aqq mudo o nome da tabela
+@Table(name = "tb_user")
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -22,6 +28,11 @@ public class User implements Serializable{
 	private String telefone;
 	private String email;
 	private String password;
+
+// instanciando associação user (1) com varios pedidos(0rder)
+//	asociacao um para muitos mapeado por cliente	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -74,8 +85,12 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+ 
+	public List<Order> getOrders() {
+		return orders;
+	}
 
-	@Override
+ 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
