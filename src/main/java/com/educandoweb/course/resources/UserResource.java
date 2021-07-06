1873e3f2eaf5ea1de3f86b67781aae37d8386b2e
Easy-System_EF <enelmofreitas@gmail.com>
2021-07-06 20:10:34 -0300

    User insert cod 200

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.service.UserService;
-
+ 
 //  1a recurso web implementado pelo recurso rest
 //  2a nome do resurso com caminho "/entitie" 
 @RestController
@@ -37,11 +39,18 @@ public class UserResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+//		return ResponseEntity.ok().body(obj);
+// retorno do codigo201 = criou;		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 
-	// anotação incluisão
-	// chamando o service
+/*
+	 anotação incluisão
+	 chamando o service
+	 os dados postados no Postman devem ter os atributos(cpos) identicos aos definidos na classe
+	 senão sai null
+*/
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = service.insert(obj);
