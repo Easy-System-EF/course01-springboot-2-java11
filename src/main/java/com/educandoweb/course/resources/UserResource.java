e21adf42e4f31ab0b1370c3d578cb4fd1263c295
Easy-System-EF <enelmofreitas@gmail.com>
2021-07-11 13:10:10 -0300

    User delete

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,8 +47,7 @@ public class UserResource {
 	}
 
 /*
-	 anotação incluisão
-	 chamando o service
+	 anotação inclusão //  chamando o metodo service
 	 os dados postados no Postman devem ter os atributos(cpos) identicos aos definidos na classe
 	 senão sai null
 */
@@ -55,5 +55,14 @@ public class UserResource {
 	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = service.insert(obj);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+ 	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		
+		
+		
 	}
 }
